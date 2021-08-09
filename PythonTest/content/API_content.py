7dcd2a32bf6c9aa657a2c6acfc7d475400d06ee7
AmazingCY <caoyu1997ch@gmail.com>
2021-08-09 22:23:20 +0800

    update python_test

---
+++ PythonTest/content/API_content.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/8/8 23:25
+# @Author : Cao yu
+# @File : API_content.py
+# @Software: PyCharm
+
+import  requests
+
+response = requests.get("http://www.baidu.com")
+print(response)
